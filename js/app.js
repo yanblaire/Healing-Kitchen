775d32a874a2e34ddb3c6e74051a7cb3bcd139bf
Salvador314 <salvador.mezalopez@fhnw.edu>
2025-06-26 11:12:28 -0600

    worked on header and contact page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
-// Navigation Bar Scrool Animation
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    var opacity = 1 - (window.scrollY / 150);
-    opacity = Math.max(0, opacity);
+// nav bar variables
+const icons = document.querySelectorAll('.icon');
+const navBar = document.querySelector('.nav-bar');
 
-    if(this.window.screenY > 500){
-        opacity = 0;
-        header.style.zIndex = 50;
-    }else{
-        header.classList.toggle('sticky', window.scrollY > 0);
-        header.style.zIndex = 30;
-    }
+// // Navigation Bar Scrool Animation
+// window.addEventListener('scroll', function() {
+//     var header = this.document.querySelector('header');
+//     var nav = this.document.getElementsByClassName("nav-icon-5");
+//     var opacity = 1 - (window.scrollY / 150);
+//     opacity = Math.max(0, opacity);
 
-    header.style.opacity = opacity;
-});
+//     if(this.window.screenY > 500){
+//         opacity = 0;
+//         header.style.zIndex = 50;
+//         nav.style.zIndex = 50;
+//     }else{
+//         header.classList.toggle('sticky', window.scrollY > 0);
+//         nav.classList.toggle('sticky', window.scrollY > 0);
+//         header.style.zIndex = 30;
+//         nav.style.zIndex = 30;
+//     }
+
+//     header.style.opacity = opacity;
+//     nav.style.opacity = opacity;
+// });
+
+// nav bar animation
+icons.forEach (icon => {  
+    icon.addEventListener('click', (event) => {
+      icon.classList.toggle("open");
+      navBar.classList.toggle("is-active");
+    });
+  });
